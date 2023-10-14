d64856253c0575f44b2ddc46e6859c4100f26a7c
Houcem Hariz <houcem.hariz@gmail.com>
2023-10-14 10:54:00 +0200

    set up the login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,17 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  inSubmission = false
+  showAlert = false
+  alertMessage = 'Please wait! Your account is being created'
+  alertColor = 'blue'
+
   credentials = { 
      email: '',
      password: ''
   }
-  constructor(){}
+  constructor(private auth : AngularFireAuth){}
   ngOnInit(): void {
     
   }
 
-  login() {
-    console.log(this.credentials);
+  async login() {
+    this.showAlert = true
+    this.alertMessage = 'Please wait! Your account is being created'
+    this.alertColor = 'blue'  
+    this.inSubmission = true
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      ) 
+    } catch (error) {
+      console.error(error)
+      this.alertMessage = 'An unexpected error occured. Please try again later'
+      this.alertColor = 'red'
+      this.inSubmission = false
+      return        
+    }
+
+    this.alertMessage = 'Success! Your account has been created'
+    this.alertColor = 'green'
+
   }
 
 }
