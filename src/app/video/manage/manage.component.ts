63d87f4c1782c5c65ee005693690e7a16e1bef23
Houcem Hariz <houcem.hariz@gmail.com>
2023-10-25 13:12:09 +0200

    prepare the form

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -15,7 +16,8 @@ export class ManageComponent implements OnInit{
   constructor(
     private router : Router, 
     private route : ActivatedRoute,
-    private clipService: ClipService){
+    private clipService: ClipService,
+    private modalService: ModalService){
 
   }
 
@@ -46,5 +48,11 @@ export class ManageComponent implements OnInit{
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.modalService.toggleModal('editClip')
   }
 }
