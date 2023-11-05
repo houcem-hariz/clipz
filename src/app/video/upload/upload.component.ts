d5843220a48eb2650d01d379c8cbbcd85e4ec6ab
Houcem Hariz <houcem.hariz@gmail.com>
2023-11-05 15:32:31 +0100

    save file in memory with ffmpeg

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -56,7 +56,7 @@ export class UploadComponent implements OnDestroy{
     this.task?.cancel()    
   }
 
-  storeFile($event: Event){
+  async storeFile($event: Event){
     this.isDragOver = false
     this.file = ($event as DragEvent).dataTransfer ?
       (($event as DragEvent).dataTransfer?.files.item(0) ?? null) :
@@ -66,6 +66,8 @@ export class UploadComponent implements OnDestroy{
     if (!this.file || this.file.type != 'video/mp4') {
       return
     }
+
+    await this.ffmpegService.getScreenshots(this.file)
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''))
     this.nextStep = true
